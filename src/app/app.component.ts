1cb7779ccfd2e6fcc3abf948e9fc96522d7ca048
abhijeet <abhijeetphophaliya@gmail.com>
2023-03-22 18:33:26 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { UserStoreService } from './services/user-store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FullStack.UI';
+
+  constructor(
+    public authService: AuthService,
+    private userStore: UserStoreService
+  ) {}
+
+  fullName: string = '';
+
+  ngOnInit(): void {
+    this.userStore.getFullNameFromStore().subscribe((val) => {
+      let fullnameFromToken = this.authService.getFullnameFromToken();
+      this.fullName = val || fullnameFromToken;
+    });
+  }
+
+  logOutUser() {
+    this.authService.signOut();
+  }
 }
